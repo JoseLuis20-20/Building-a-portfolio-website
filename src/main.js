847e989ad05f479139effb8d8b953f1eaf40e0a0
JoseLuis20-20 <joseluis.mmx@gmail.com>
2023-08-02 04:27:47 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import '/modern-normalize.css';
+import '/style.css';
+import '/header.css';
+import '/hero.css';
+import '/about.css';
+import '/featured.css';
+import '/work.css';
+import '/contact.css';
+import '/footer.css';
+import '/mobile-nav.css';
+import '/utils.css';
+
+
+import mobileNav from './utils/mobile-nav';
+import darkMode from './utils/dark-mode';
+import lazyLoading from './utils/lazy-loading';
+
+mobileNav();
+darkMode();
+lazyLoading();
